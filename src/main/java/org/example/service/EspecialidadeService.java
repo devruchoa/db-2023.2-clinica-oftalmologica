7c39fbe77c910c049f4fc1a9c824d64b322e9aeb
devruchoa <ronaldo.uchoa.pessoal@gmail.com>
2023-12-04 19:51:01 -0300

    feat: create EspecialidadeService

---
+++ src/main/java/org/example/service/EspecialidadeService.java
@@ -0,0 +1,8 @@
+package org.example.service;
+
+import org.example.dao.EspecialidadeDAO;
+
+public class EspecialidadeService extends EspecialidadeDAO {
+
+    // TODO: Here you can implement business logic for Especialidade
+}
